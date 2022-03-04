164952a82a816ce96ca3c7e6247d6ae593ad08aa
Huginn-Muninn <tomas.orri.arnarsson@gmail.com>
2022-03-04 20:26:30 +0000

    check

--- Bank/test_Account.py
+++ Bank/test_Account.py
@@ -21,8 +21,8 @@ def test_number_of_accounts_increases_on_creation(two_accounts):
 
 # Write your tests below here.
 def test_amount_start():
-    account = Account(2500)
-    assert account.amount == 2500
+    account1 = Account(2500)
+    assert account1.amount == 2500
     
 def test_withdraw():
     account = Account(2500)
@@ -41,5 +41,9 @@ def test_transfer():
     assert account1.amount + account2.amount == 5000
     assert account1.amount == 1500
     assert account2.amount == 3500
+
+def test_str():
+    account = Account(2500)
+    assert account.__str__() == "000001"
     
     
